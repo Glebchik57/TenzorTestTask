1b8cee40afaa58094bbfb25dd92f05889d9c7ee8
Glebchik57 <g.sevostyanov@inbox.ru>
2024-08-10 18:31:54 +0200

    tests: testcase2 was created

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -1,8 +1,10 @@
-from pages.locators import MainPageLocators, TensorPageLocators, AboutPageLocators
+from pages.locators import MainPageLocators, TensorPageLocators, AboutPageLocators, ContacsPageLocators
 from pages.main_page import MainPage
 from pages.contact_page import ContactsPage
 from pages.tensor_page import TensorPage
 from pages.about_page import AboutPage
+
+import time
 
 
 class TestCase1():
@@ -30,3 +32,24 @@ class TestCase1():
         page = AboutPage(browser, AboutPageLocators.SELF_LINK)
         page.open()
         page.pictures_have_same_sizes()
+
+
+class TestCase2():
+    def test_should_be_right_region_partners_there_are(self, browser):
+        page = MainPage(
+            browser,
+            MainPageLocators.SELF_LINK
+        )
+        page.open()
+        page.go_to_contacts_page()
+        contact_page = ContactsPage(browser, browser.current_url)
+        contact_page.region_is_right_partners_there_are()
+
+    def test_change_region(self, browser):
+        contact_page = ContactsPage(browser, ContacsPageLocators.SELF_LINK)
+        contact_page.open()
+        old_inf = contact_page.get_information_current_region()
+        contact_page.change_region()
+        time.sleep(10)
+        new_inf = contact_page.get_information_current_region()
+        contact_page.check_region_was_changed(old_inf, new_inf)
